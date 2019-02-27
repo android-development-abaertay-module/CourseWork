a1b1f6f36e959d95c219fc7d89440fef6e5ce020
Bodovix <stbbap@gmail.com>
2019-02-27 12:03:44 +0000

    Custom navigation icons added to menu activity

--- app/src/main/java/com/example/coursework/view/MenuActivity.java
+++ app/src/main/java/com/example/coursework/view/MenuActivity.java
@@ -54,12 +54,13 @@ public class MenuActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
+    //This Creates the top right settings menu (menu) . don't need for now
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        //Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -68,10 +69,10 @@ public class MenuActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
