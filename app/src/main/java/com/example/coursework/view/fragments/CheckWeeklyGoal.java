0a6966637515f3afdc7ef6b1629c43fed28766b8
Bodovix <stbbap@gmail.com>
2019-03-12 09:06:10 +0000

    added propertiest checkGoalWeekly Fragment

--- app/src/main/java/com/example/coursework/view/fragments/CheckWeeklyGoal.java
+++ app/src/main/java/com/example/coursework/view/fragments/CheckWeeklyGoal.java
@@ -8,13 +8,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.coursework.R;
+import com.example.coursework.model.GoalWeekly;
+import com.example.coursework.model.enums.Grades;
 import com.example.coursework.viewmodel.CheckGoalsVM.CheckWeeklyGoalViewModel;
 
 public class CheckWeeklyGoal extends Fragment {
 
     private CheckWeeklyGoalViewModel mViewModel;
+    private GoalWeekly goalWeekly;
+    private int numberSportAchived;
+    private int numberBoulderAchived;
+    private Grades avgSportAchived;
+    private Grades avgBoulderAchived;
+    private ProgressBar noSportAchivedPb;
+    private ProgressBar noBoulderAchivedPb;
+    private ProgressBar avgSportAchivedDisplay;
+    private ProgressBar avgBoulderAchivedDisplay;
 
     public static CheckWeeklyGoal newInstance() {
         return new CheckWeeklyGoal();
@@ -24,13 +36,22 @@ public class CheckWeeklyGoal extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.check_weekly_goal_fragment, container, false);
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(CheckWeeklyGoalViewModel.class);
-        // TODO: Use the ViewModel
+
+
+        //region [Declare properties]
+        noSportAchivedPb = getView().findViewById(R.id.checkGoalWeeklyNoSportPB);
+        noBoulderAchivedPb = getView().findViewById(R.id.checkGoalWeeklyNoBoulderPB);
+        avgSportAchivedDisplay = getView().findViewById(R.id.checkGoalWeeklyAvgSportTV);
+        avgBoulderAchivedDisplay = getView().findViewById(R.id.checkGoalWeeklyAvgBoulderTV);
+
+        //endregion
     }
 
 }
